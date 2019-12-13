c3ab9ad1a1ddd315b2332a0dc2bd3eac5103c48c
NeckChin <nick.chen053100@gmail.com>
2019-12-12 17:02:24 -0800

    reverting to prev structure

--- index.php
+++
@@ -1,8 +0,0 @@
-
-<head>
-<!--<meta http-equiv="refresh" content="0; URL=main">-->
-<meta http-equiv="refresh" content="0; URL=/main/">
-</head>
-
-<!-- Normally set to URL=home.php -->
-<!-- When needed, change the URL to the page you want "apousc.org" to redirect to. For example, if there site is not working, have it redirect to URL=underconstruction.html -->
