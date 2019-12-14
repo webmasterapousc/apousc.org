e92340816a5dc1440462d2989331e9ac39bb2de2
NeckChin <nick.chen053100@gmail.com>
2019-12-13 16:34:08 -0800

    test 2.2

--- include/database.php
+++ include/database.php
@@ -600,7 +600,8 @@ class MySQLDB
 	function addNewAnnouncement($title, $body, $date)
 	{
 		$q = "INSERT INTO `" . TBL_ANNOUNCEMENTS . "` VALUES ('','$title','$date','$body')";
-		return mysql_query($q, $this->connection);
+		$result = mysql_query($q, $this->connection);
+		return $result;
 	}
 
 	/**
