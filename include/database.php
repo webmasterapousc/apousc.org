09cb39a1159bf44f30e2d47994389805e14c819f
NeckChin <nick.chen053100@gmail.com>
2019-12-16 06:42:49 -0800

    more test

--- include/database.php
+++ include/database.php
@@ -26,7 +26,7 @@ class MySQLDB
 	function MySQLDB()
 	{
 		/* Make connection to database */
-		$this->connection = mysql_connect(DB_SERVER, "neckchin", "root") or die(mysql_error());
+		$this->connection = mysql_connect(DB_SERVER, DB_USER, DB_PASS) or die(mysql_error());
 		mysql_select_db(DB_NAME, $this->connection) or die(mysql_error());
 		
 		/**
@@ -600,6 +600,8 @@ class MySQLDB
 	function addNewAnnouncement($title, $body, $date)
 	{
 		$q = "INSERT INTO `" . TBL_ANNOUNCEMENTS . "` VALUES ('','$title','$date','$body')";
+		file_put_contents('php://stderr', print_r("\n\nERROR DEBUG: $q is " . $q . "\n\n\n", TRUE));
+
 		return mysql_query($q, $this->connection);
 	}
 
