82588bbc1d9d009ec490ead6d3292522f538354b
NeckChin <nick.chen053100@gmail.com>
2019-12-16 07:47:05 -0800

    nvm

--- include/database.php
+++ include/database.php
@@ -603,7 +603,7 @@ INSERT INTO `events` (`ID`, `name`, `type`, `desc`, `start`, `end`, `hours`, `ma
 	 */
 	function addNewAnnouncement($title, $body, $date)
 	{
-		$q = "INSERT INTO " . TBL_ANNOUNCEMENTS . " VALUES (NULL, '$title','$date','$body')";
+		$q = "INSERT INTO " . TBL_ANNOUNCEMENTS . " VALUES ('$title','$date','$body')";
 		//file_put_contents('php://stderr', print_r("\n\nERROR DEBUG: q is " . $q . "\n\n\n", TRUE));
 
 		return mysql_query($q, $this->connection);
