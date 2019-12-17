841c92363a69f1dac1b582defbb14cdcae42b5b9
NeckChin <nick.chen053100@gmail.com>
2019-12-17 08:31:39 -0600

    add to google calendar button on events page implemented

---
+++ members/delete_entry.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Process.php
+ * 
+ * The Process class is meant to simplify the task of processing
+ * user submitted forms, redirecting the user to the correct
+ * pages if errors are found, or if form is successful, either
+ * way. Also handles the logout procedure.
+ *
+ * Written by: Jpmaster77 a.k.a. The Grandmaster of C++ (GMC)
+ * Last Updated: August 19, 2004
+ *
+ * Modified by: Brad Ramos (bradleyRamos@gmail.com)
+ * Last Updated: November 2011
+ */
+include("include/session.php");
+
+
+$id=$_POST['id'];
+$delete = "DELETE FROM comments WHERE id=$id";
+$result = mysql_query($delete) or die(mysql_error());
+
+
+?>
