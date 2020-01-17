5e6d008d07a324e595c32573a27fb95d15c4dda1
Nick Chen <nickchen@usc.edu>
2020-01-17 02:01:29 -0600

    test phil change

--- members/event_page.php
+++ members/event_page.php
@@ -528,8 +528,8 @@ if (mysql_num_rows($result) > 0) {
 
             		($session->position == 16) ||
 
-            		($session->position == 17 && $req_event_info['type'] == 0) ||
-            		($session->position == 17 && $req_event_info['type'] == 7) ||
+            		($session->position == 17 && $req_event_info['type'] == "0") ||
+            		($session->position == 17 && $req_event_info['type'] == "7") ||
 
 
             		($session->position == 18) ||
@@ -752,6 +752,7 @@ if (
 	($session->position == 16) ||
 
 	($session->position == 17 && $req_event_info['type'] == 0) ||
+	($session->position == 17 && $req_event_info['type'] == 7) ||
 
 	($session->position == 18) ||
 
