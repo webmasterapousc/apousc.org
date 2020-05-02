fa44d39214f265ad11d09cdb23599f1bd0c7b07e
Nick Chen <nickchen@usc.edu>
2020-05-02 00:23:18 -0500

    added jessica woon's application details to the election docs page

--- election/election_documents.php
+++ election/election_documents.php
@@ -119,7 +119,13 @@ include_once("include/topnav.php");
 
           <h3>VP of Communications</h3>
             <ul>
-                
+              <li>
+                <li><b>Jessica Woon</b></li>
+                <ul><a href = "docs/election/fall20/finance/jw_application.docx"> Application </a></ul>
+                <ul><a href = "docs/election/fall20/finance/jw_resume.docx"> Resume </a></ul>
+                <ul><a href = "docs/election/fall20/finance/jw_schedule.png"> Schedule </a></ul>
+
+              </ul>
             </ul>
                   
           <h3>Interchapter Chair</h3>
