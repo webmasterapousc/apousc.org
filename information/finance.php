c610fbec74907b9f3a1e5028b5e066c6c29e0618
Ivan Peng <ipeng@usc.edu>
2021-01-19 21:11:43 -0800

    Update finance.php
    - updated budget link, $ amounts, and payment plan dates

--- information/finance.php
+++ information/finance.php
@@ -33,29 +33,29 @@ include_once("include/topnav.php");
 
                 <h3>Spring 2021 Information</h3>
                 <ul>
-                    <li><a href='https://docs.google.com/spreadsheets/d/1o3LOmUChGxSRILlPCeNK2OertuMHmrQjFfw9-JjTyfM/edit#gid=1809411573'>Budget</a></li>
+                    <li><a href='https://docs.google.com/spreadsheets/d/1i0HwDQ_ZVqr24Lv5dOUhTYxk0pgVXH9wwkgeSA-VSrI/edit#gid=2132359598'>Budget</a></li>
                     <li>Venmo: @apoakusc</li>
                     <li>Email: <a href="mailto:finance.apousc@gmail.com">finance.apousc@gmail.com</a></li>
                 </ul>
                 
                 <h3>Dues</h3>
                 <p>Please contact Lindsey Yu or Felicia Tejawinata if you need additional financial accommodations for our payment plans.</p>
-		<h4><b>Actives: $150 regular, $135 early</b></h4>
-                <h4><b>Associates: $55 regular, $49 early</b></h4>
-		<h4><b>New Members: $325 regular, n/a early</b></h4>
+		<h4><b>Actives: $35 regular, $30 early</b></h4>
+<!--                 <h4><b>Associates: $55 regular, $49 early</b></h4> -->
+		<h4><b>New Members: $150 regular, n/a early</b></h4>
 		<p><b>Actives Payment Plan (Regular)</b></p>	
                 <ul>
-                    <li>Monday, February 10: $70</li>
-                	<li>Monday, March 2: $40</li>
-                    <li>Monday, March 30: $40</li>
+                    <li>Monday, February 22: $35</li>
+<!--                 	<li>Monday, March 2: $40</li>
+                    <li>Monday, March 30: $40</li> -->
             	</ul>
             	<p><b>Actives Payment Plan (Early)</b></p>	
                 <ul>
-                    <li>Monday, before February 10: $65</li>
-                	<li>Monday, February 24: $35</li>
-                    <li>Monday, March 23: $35</li>
+                    <li>Monday, February 22 (Finish Recruitment Requirements): $30</li>
+<!--                 	<li>Monday, February 24: $35</li>
+                    <li>Monday, March 23: $35</li> -->
             	</ul>
-            	<p><b>Associates Payment Plan (Regular)</b></p>	
+<!--             	<p><b>Associates Payment Plan (Regular)</b></p>	
                 <ul>
                     <li>Monday, February 10: $25</li>
                 	<li>Monday, March 2: $15</li>
@@ -66,12 +66,18 @@ include_once("include/topnav.php");
                     <li>Monday, before February 10: $23</li>
                 	<li>Monday, February 24: $13</li>
                     <li>Monday, March 23: $13</li>
+            	</ul> -->
+			<p><b>New Members Payment Plan (Regular)</b></p>	
+                <ul>
+                    <li>Monday, February 22: $50</li>
+                	<li>Monday, March 8: $50</li>
+                    <li>Monday, March 22: $50</li>
             	</ul>
 				<p><b>*If you do not pay your dues and do not communicate with the VPs, we will have to freeze your account until the matter is resolved.</b></p>
 				
 				<h3>Reimbursements</h3>
 				
-				<p>In order to get reimbursed, please send your receipts to Lindsey Yu, Felicia Tejawinata, or finance.apousc@gmail.com.</p>
+				<p>In order to get reimbursed, please send your receipts to Lindsey Yu or Felicia Tejawinata at finance.apousc@gmail.com.</p>
 				
 <br />
 				
