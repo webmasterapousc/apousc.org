8f44b82ffdd3522a69378a844a117ff92791f362
Ivan Peng <ipeng@usc.edu>
2021-02-16 15:57:49 -0800

    Update event_page.php
    edit gcal icon size

--- members/event_page.php
+++ members/event_page.php
@@ -871,7 +871,7 @@ if (
         echo "<a href='https://www.google.com/calendar/render?action=TEMPLATE&text=$eventName&dates=$eventStart/$eventEnd&details=$eventDesc&location=$eventLoc&sf=true&output=xml');
         \">Click here to add event to Google Calendar</a>";
 	echo "<a href='https://www.google.com/calendar/render?action=TEMPLATE&text=$eventName&dates=$eventStart/$eventEnd&details=$eventDesc&location=$eventLoc&sf=true&output=xml' 
-		rel='external' title='Add to your google calendar'><img src='../img/google-calendar-icon.png' height='32' width='32' alt='Gcal' /></a>";
+		rel='external' title='Add to your google calendar'><img src='../img/google-calendar-icon.png' height='64' width='80' alt='Gcal' /></a>";
         /*************** USER COMMENTS *********************/
 # Display the current list of comments
 
