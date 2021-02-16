97ce54b449782226b7fa05e4a23a0b1246e3242c
Ivan Peng <ipeng@usc.edu>
2021-02-16 15:34:10 -0800

    Update event_page.php
    added calendar icon

--- members/event_page.php
+++ members/event_page.php
@@ -869,7 +869,7 @@ if (
         $eventStart = gmdate("Ymd\THis\Z", strtotime($eventStart));
         $eventEnd = gmdate("Ymd\THis\Z", strtotime($eventEnd));
         echo "<a href='https://www.google.com/calendar/render?action=TEMPLATE&text=$eventName&dates=$eventStart/$eventEnd&details=$eventDesc&location=$eventLoc&sf=true&output=xml');
-        \">Click here to add event to Google Calendar</a>";
+        \"><img src="../img/google-calendar-icon.png"><strong><p style:"font-size: 20px;">Click here to add event to Google Calendar</p></strong></a>";
         /*************** USER COMMENTS *********************/
 # Display the current list of comments
 
