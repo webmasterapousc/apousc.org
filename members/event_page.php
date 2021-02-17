6d7d0e23a276aca3361db04e17abc8c53c7c8c59
Ivan Peng <ipeng@usc.edu>
2021-02-16 16:01:57 -0800

    Update event_page.php
    edit gcal size

--- members/event_page.php
+++ members/event_page.php
@@ -869,8 +869,7 @@ if (
         $eventStart = gmdate("Ymd\THis\Z", strtotime($eventStart));
         $eventEnd = gmdate("Ymd\THis\Z", strtotime($eventEnd));
         echo "<a href='https://www.google.com/calendar/render?action=TEMPLATE&text=$eventName&dates=$eventStart/$eventEnd&details=$eventDesc&location=$eventLoc&sf=true&output=xml' 
-		rel='external' title='Add to your google calendar'><img src='../img/google-calendar--v1.png' 
-// 		height='64' width='80' 
+		rel='external' title='Add to your google calendar'><img src='../img/google-calendar--v1.png' height='32' width='32' 
 		alt='Gcal' /></a>";
 	echo "<a href='https://www.google.com/calendar/render?action=TEMPLATE&text=$eventName&dates=$eventStart/$eventEnd&details=$eventDesc&location=$eventLoc&sf=true&output=xml');
         \">Click here to add event to Google Calendar</a>";
