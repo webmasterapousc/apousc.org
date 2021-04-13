7e5de159a3f1389d671aeb72f2882a12ebe39511
Ivan Peng <ipeng@usc.edu>
2021-04-12 19:47:29 -0700

    added election candidate eval form

--- election/nominations.php
+++ election/nominations.php
@@ -62,7 +62,12 @@ include_once("include/topnav.php");
 				<p>Please review the <a href="https://drive.google.com/file/d/1Y8UWvkfbvm5vKxD8qb0UGVkbQytIj8ew/view?usp=sharing">Officer Descriptions</a>, 
 					<a href="https://drive.google.com/open?id=13ebg2IfQ2B013lQxT5ZO1FjwKA4g-4k9">Bylaws</a>, and <a href="https://drive.google.com/file/d/12XU7bJEfez57oS-Bz9YX54yJQcB9cJJI/view?usp=sharing">Officer Election Policy</a> 
 					for procedures regarding elections and for a list of each position's responsibilities. If you wish to decline your nomination, please click "[Decline]" next to your name, or email <a href="mailto:webmaster.apousc@gmail.com">webmaster.apousc@gmail.com</a> 
-					so that the website administrator may decline on your behalf. <br/><br/><strong>If you are accepting your nominations, please be sure to submit the <a href="https://docs.google.com/document/d/1uVfEY7P9IcKabiNCmc2bUr9hYiZR50SeSFyR710v2xk/edit">E-Board Application</a> by 11:59 PM on Wednesday, April 14. 
+					so that the website administrator may decline on your behalf. 
+					
+					<br/><br/>If you would like to comment on a candidate during elections, but would like to stay anonymous to the chapter, 
+					please fill out the <a href="https://forms.gle/V18LS1ijSPFjgvW76APO">Election Candidate Eval Form</a>
+					
+					<br/><br/><strong>If you are accepting your nominations, please be sure to submit the <a href="https://docs.google.com/document/d/1uVfEY7P9IcKabiNCmc2bUr9hYiZR50SeSFyR710v2xk/edit">E-Board Application</a> by 11:59 PM on Wednesday, April 14. 
 					</strong></br></br><strong>If you would like to apply for an appointed position, please submit your <a href="https://docs.google.com/document/d/1TP5goDQeJ1hpN6Z9H2wlY6DAevSg_3r2_hMZnui6f6k/edit">A-Board Application </a> 
 						by 11:59 PM on Monday, April 26.</strong></p> 
 				
