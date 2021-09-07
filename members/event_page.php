49e823d96ddb4e4bc17b0e2ed7156a94fc26a985
Kelly Ma <kjma@usc.edu>
2021-09-06 22:42:10 -0700

    gabe

--- members/event_page.php
+++ members/event_page.php
@@ -295,7 +295,7 @@ if (mysql_num_rows($result) > 0) {
         //alumni liason
         ($session->username == "mwu20216" && $req_event_info['type'] == 6) || 
         //historian
-        ($session->username == "ggenito") && $req_event_info['type'] == 5) ||
+        ($session->username == "ggenito" && $req_event_info['type'] == 5) ||
         //philanthropy chairs
         (($session->username == "chengea" || $session->username == "jwoon") && ($req_event_info['type'] == 7 || $req_event_info['type'] == 0)) ||
         //directors of recruitment
