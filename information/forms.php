a5afeca7cc1723b571d8c92bb269f98ea196a7c5
Kelly Ma <kjma@usc.edu>
2021-09-13 12:43:43 -0700

    Added service feedback form

--- information/forms.php
+++ information/forms.php
@@ -32,11 +32,12 @@ include_once("include/topnav.php");
 			<p>If you don't see what you're looking for, just contact an officer!</p>
 			<p>If you want to upload or update a document on this page feel free to contact webmaster.apousc@gmail.com!</p>                	
 					<ul>
-						<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSeO9JlApSE76RKqHGlITzFm-WjwCDc41TDNuSz6bIQWVyMkYA/viewform">Open Evaluation Form</a></li>
+<!-- 						<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSeO9JlApSE76RKqHGlITzFm-WjwCDc41TDNuSz6bIQWVyMkYA/viewform">Open Evaluation Form</a></li>
 		       				<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSd8_c4TqemjrAvio0DxwyMUFv-q9fcNhhzd85PrclDFJz7MJw/viewform">Event Evaluation Form</a></li>
 						<li><a href='https://docs.google.com/forms/d/e/1FAIpQLSftdqT-xgSpVWNHtkmo7T4_L4NHbxt3iZlROEMcPVoxzlGUnw/viewform' target = "_blank">Diversity and Inclusion Survey</a></li>
 					  	<li><a href='https://forms.gle/jhC3w4TQ5X8grg1C7' target = "_blank">Diversity & Inclusion Event Reflection Form</a></li>
-						<li><a href="https://drive.google.com/file/d/1lNKrfgsSEAuOeZdyZPvuFWw37KZqyM_v/preview?format=pdf" target = "_blank">Outside Service Hours Log</a></li>
+						<li><a href="https://drive.google.com/file/d/1lNKrfgsSEAuOeZdyZPvuFWw37KZqyM_v/preview?format=pdf" target = "_blank">Outside Service Hours Log</a></li> -->
+						<li><a href="https://forms.gle/myvywQUroWRXGGvF9">APO Service Feedback (Fall 2021)</a></li>
             					<li><a href="https://www.facebook.com/APO-Compliments-Spring-2020-115121043370901/">APO Compliments Form</a></li>
 						<li><a href="https://docs.google.com/forms/d/e/1FAIpQLSdPnubmuQwhKJhf4JtFFxzTDjkflzsvxbmO2DUSQHdlkOI6PA/viewform">TOWNHALL - Bylaw Changes Form</a></li>						
 					</ul>
