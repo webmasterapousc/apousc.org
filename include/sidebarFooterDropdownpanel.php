f3ea05f6cb24823a35cc2ef4bb70ff20d3794ff6
Kelly Ma <kjma@usc.edu>
2021-10-12 22:19:47 -0700

    added new_members.php

--- include/sidebarFooterDropdownpanel.php
+++ include/sidebarFooterDropdownpanel.php
@@ -118,7 +118,7 @@ HERE;
 		<li><a href="../rosters/roster.php">All Members</a></li>
 		<li><a href="../rosters/excomm_a.php">Executive Committee</a></li>
 		<li><a href="../rosters/excomm_p.php">New Member Ex Comm</a></li>
-		<li><a href="">don't click me!!!</a></li>
+		<li><a href="../rosters/new_members.php">don't click me!!!</a></li>
 		<li><a href="../rosters/alumni.php">Alumni</a></li>
 		<!--<li><a href="https://airtable.com/shrl0bWZ6eINW28Ec" target="_blank">Alumni Networking Database</a></li>-->
 <!-- 		<li><a href="../rosters/family_tree.php?user=all">Family Trees</a></li> -->
